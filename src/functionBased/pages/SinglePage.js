37fda1646e5202bdba01b21546588c02cdbc8839
dadadon <davianberoni@yahoo.com>
2022-09-23 11:45:33 -0500

    Made changes to files

--- src/functionBased/pages/SinglePage.js
+++ src/functionBased/pages/SinglePage.js
@@ -8,13 +8,13 @@ const SinglePage = () => {
       slug: "about-app",
       title: "About the App",
       description: 
-      "In this app, you can add, delete, submit and edit items. To edit items, simply double click on it. Once you are done, press the enter key to resubmit. This app will persist your data in the browser local storage. So whether you reload, close your app or reopened it, you still have access to your to-dos items.",
+      "This app allows you to add, delete, submit, and edit things. Simply double-click an item to edit it. When you're finished, hit the enter key to resubmit. Your data will be saved in the browser's local storage by this app. So, whether you reload, close, or reopen your app, you will still have access to your to-do items.",
     },
     {
       slug: "about-author",
       title: "About the Author",
       description: 
-      "This app was developed by Ibas Majid, a self taught web developer and a technical writer. He is opened to freelance Gig. So go ahead and connect with ibas on Twitter @ibaslogic.",
+      "This app was developed by Davian Beroni, I've been coding for 8 years, beginning with mobile development but quickly discovering a love for web development.",
     },
   ];
 
